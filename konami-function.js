a289d791065ca72bf0a3d58999b7f3e1687afddb
Samuel Arguello <sam.rene12@gmail.com>
2022-03-09 23:19:58 -0600

    fixed display functionality on programs success and failure

--- konami-function.js
+++ konami-function.js
@@ -1,5 +1,29 @@
 "use strict";
 
+// ========== countdown logic ==========
+
+function gameOver(){
+    $(".spooky-div").css("background", "red");
+    $("img").css("visibility", "hidden");
+    $(".message").html("Looks like you didn't make it...<br>better luck next time...")
+    $("button").removeClass("hidden");
+    $("#directions").addClass("hidden");
+}
+
+let gameTime = setTimeout(gameOver, 5000)
+
+// allows the user to refresh the game when the button is clicked
+function refreshPage(){
+    window.location.reload();
+}
+
+// disables the movement caused by hitting the arrow keys
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 // ========== konami code logic ==========
 
 // up = 38
@@ -41,8 +65,11 @@ $(document).keyup(function(e){
 
 // function takes in the array of potential code and checks the length of characters and if the characters match the characters of the konami code
 function verifyCodeLength(potentialCode){
-    if (potentialCode.length === 10){
-
+    if (potentialCode.length === 10 && JSON.stringify(userInput) === JSON.stringify(konamiCode)) {
+            clearTimeout(gameTime);
+            $(".message").html("Congratulations you survived! For now... <br> If you enjoyed this game check out the code at: <br> <a href='https://github.com/Sarguello12' target='_blank''>github.com/Sarguello12</a> ");
+            $("button").removeClass("hidden");
+            $("#directions").addClass("hidden");
         }
     }
 
